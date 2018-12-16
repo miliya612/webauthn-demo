08ec560967c394e250e8f7d6785a066fb2542946
miliya12 <t.miliya612@gmail.com>
2018-12-16 21:31:56 +0900

    fix/support cors

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 	"github.com/miliya612/webauthn-demo/presentation/routes"
 	"github.com/miliya612/webauthn-demo/registry"
 	"log"
@@ -20,7 +22,14 @@ func init() {
 func main() {
 	r := registry.Registration{}
 	router := routes.NewRouter(r.RegisterCredentialHandler())
+	corsMw := mux.CORSMethodMiddleware(router)
+	router.Use(corsMw)
+
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+
 
 	log.Printf("server started at: %v", time.Now())
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(originsOk, headersOk, methodsOk)(router)))
 }
--- presentation/middleware/accessCtrl.go
+++ presentation/middleware/accessCtrl.go
@@ -4,8 +4,9 @@ import "net/http"
 
 func AccessControl(h http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+		//w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
+		//w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+		//w.Header().Set("Access-Control-Allow-Origin", "*")
 
 		if r.Method == "OPTIONS" {
 			return
